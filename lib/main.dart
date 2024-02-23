0c2ee946336e908422ee1a38401c634c6db0fe36
Englishman <56271563+RomanEnglishman@users.noreply.github.com>
2024-02-23 11:21:09 +0600

    final

---
+++ lib/main.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'UserInfo.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: RegistrationForm(),
+    );
+  }
+}
+
+class RegistrationForm extends StatefulWidget {
+  @override
+  _RegistrationFormState createState() => _RegistrationFormState();
+}
+
+class _RegistrationFormState extends State<RegistrationForm> {
+  bool _hidePass = true;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _lifeHistoryController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  final List<String> _countries = ['Russia', 'Ukraine', 'Germany', 'France'];
+  String _selectedCountry = 'Russia';
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _phoneNumberController.dispose();
+    _emailController.dispose();
+    _lifeHistoryController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registration Form'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                    labelText: 'Full Name',
+                    border: OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.clear),
+                      onPressed: () => _nameController.clear(),
+                    ),
+                  ),
+                  validator: (value) =>
+                      value!.isEmpty ? 'Please enter your full name' : null,
+                ),
+                SizedBox(height: 10.0),
+                TextFormField(
+                  controller: _phoneNumberController,
+                  decoration: InputDecoration(
+                    labelText: 'Phone Number',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.phone,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  validator: (value) =>
+                      value!.isEmpty ? 'Phone number is required' : null,
+                ),
+                SizedBox(height: 10.0),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) =>
+                      !value!.contains('@') ? 'Enter a valid email' : null,
+                ),
+                SizedBox(height: 10.0),
+                DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    labelText: 'Country',
+                    border: OutlineInputBorder(),
+                  ),
+                  value: _selectedCountry,
+                  items:
+                      _countries.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedCountry = value!;
+                    });
+                  },
+                  validator: (value) =>
+                      value == null ? 'Please select a country' : null,
+                ),
+                SizedBox(height: 10.0),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                          _hidePass ? Icons.visibility_off : Icons.visibility),
+                      onPressed: () => setState(() => _hidePass = !_hidePass),
+                    ),
+                  ),
+                  obscureText: _hidePass,
+                  validator: (value) => value!.length < 8
+                      ? 'Password must be at least 8 characters'
+                      : null,
+                ),
+                SizedBox(height: 10.0),
+                TextFormField(
+                  controller: _confirmPasswordController,
+                  decoration: InputDecoration(
+                    labelText: 'Confirm Password',
+                    border: OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                          _hidePass ? Icons.visibility_off : Icons.visibility),
+                      onPressed: () => setState(() => _hidePass = !_hidePass),
+                    ),
+                  ),
+                  obscureText: _hidePass,
+                  validator: (value) => value != _passwordController.text
+                      ? 'Passwords do not match'
+                      : null,
+                ),
+                SizedBox(height: 20.0),
+                OutlinedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                    
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => UserInfo(
+                            name: _nameController.text,
+                            phoneNumber: _phoneNumberController.text,
+                            email: _emailController.text,
+                            country: _selectedCountry,
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text('Register'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
